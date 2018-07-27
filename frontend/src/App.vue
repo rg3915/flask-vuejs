164be3ce8d842bf874748660ef22dd2af00ed5e6
regis <regis.santos.100@gmail.com>
2018-07-26 23:49:41 -0300

    frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img height="100px" src="./assets/logo.png">
+    <!-- <HelloWorld/> -->
+    <Product/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Product from './components/Product'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Product
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="product">
+    <h1>Produtos</h1>
+    <table class="pure-table">
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products">
+          <td>{{ product.name }}</td>
+          <td style="text-align:right">{{ formatPrice(product.price) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProducts(){
+      axios.get('http://localhost:5000/product/').then((result) => {
+        console.log(result)
+      this.products = result.data
+      })
+    },
+    formatPrice(value) {
+      let val = (value/1).toFixed(2).replace('.', ',')
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+    }
+  },
+  mounted(){
+    this.getProducts()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+table {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
